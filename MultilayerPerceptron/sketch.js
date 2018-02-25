c226c4085da15507e448bfcd17a6ab8cd8f1dc55
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-02-25 10:47:07 +0100

    Implement map function and cleaning the code

--- MultilayerPerceptron/sketch.js
+++ MultilayerPerceptron/sketch.js
@@ -3,7 +3,32 @@ var brain;
 
 function setup(){
 
-	brain = new NeuralNetwork(3, 3, 1);
+	let a = new Matrix(2, 2);
+	a.randomize();
+	a.print();
+
+	function doubleIt(x){
+		return x * 2;
+	}
+
+	a.map(doubleIt);
+	a.print();
+
+	/*let a = new Matrix(2,3);
+	let b = new Matrix(3,2);
+	a.randomize();
+	b.randomize();
+	console.table(a.matrix);
+	console.table(b.matrix);
+
+	let c = a.multiply(b);
+	console.table(c.matrix);
+
+	let d = new Matrix(2,3);
+	d.randomize();
+	let e = d.transpose();
+	console.table(d.matrix);
+	console.table(e.matrix);*/
 }
 
 function draw(){
