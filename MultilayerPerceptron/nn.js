3d658239fa16cdea39205f16ef4990d95b43add4
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-03-07 15:15:05 +0100

    Rewriting the activation function, make a Canvas to visualize XOR problem

--- MultilayerPerceptron/nn.js
+++ MultilayerPerceptron/nn.js
@@ -1,56 +1,80 @@
-function sigmoid(x){
-	return 1 / (1 + Math.exp(-x));
+class ActivationFunction{
+  constructor(func, dfunc){
+    this.func = func;
+    this.dfunc = dfunc;
+  }
 }
 
-function dsigmoid(y){
-	//return sigmoid(x) * (1 - sigmoid(x));
-	return y * (1 - y);
-}
+let sigmoid = new ActivationFunction(
+  x => 1 / (1 + Math.exp(-x)),
+  y => y * (1- y)
+);
 
-class NeuralNetwork{
-	constructor(input_nodes, hidden_nodes, output_nodes){
-		this.input_nodes = input_nodes;
-		this.hidden_noddes = hidden_nodes;
-		this.output_nodes = output_nodes;
+let tanh = new ActivationFunction(
+  x => Math.tanh(x),
+  y => 1-(y*y)
+);
 
-		this.weights_ih = new Matrix(this.hidden_nodes, this.input_nodes);
-		this.weights_ho = new Matrix(this.output_nodes, this.hidden_nodes);
-		this.weights_ih.randomize();
-		this.weights_ho.randomize();
 
-		this.bias_h = new Matrix(this.hidden_nodes,1);
-		this.bias_o = new Matrix(this.output_nodes,1);
-		this.bias_h.randomize();
-		this.bias_o.randomize();
-		this.learning_rate = 0.1;
-	}
-	predict(input_array){
-		//Generating the hidden outputs
-		let inputs = Matrix.fromArray(input_array);
-		let hidden = new Matrix.multiply(this.weights_ih, inputs);
-		hidden.add(this.bias_h);
-		//activation function
-		hidden.map(sigmoid);
-		//Generate output
-		let output = Matrix.multiply(this.weights_ho, hidden);
-		output.add(this.bias_o);
-		output.map(sigmoid);
-		//Sening it back to array
-		return out.toArray();
-	}
+class NeuralNetwork {
+  constructor(input_nodes, hidden_nodes, output_nodes) {
+    this.input_nodes = input_nodes;
+    this.hidden_nodes = hidden_nodes;
+    this.output_nodes = output_nodes;
 
-	train(input_array, target_array) {
+    this.weights_ih = new Matrix(this.hidden_nodes, this.input_nodes);
+    this.weights_ho = new Matrix(this.output_nodes, this.hidden_nodes);
+    this.weights_ih.randomize();
+    this.weights_ho.randomize();
+
+    this.bias_h = new Matrix(this.hidden_nodes, 1);
+    this.bias_o = new Matrix(this.output_nodes, 1);
+    this.bias_h.randomize();
+    this.bias_o.randomize();
+    this.setLearningRate();
+
+    this.setActivationFunction();
+
+  }
+
+  predict(input_array) {
+
     // Generating the Hidden Outputs
     let inputs = Matrix.fromArray(input_array);
     let hidden = Matrix.multiply(this.weights_ih, inputs);
     hidden.add(this.bias_h);
     // activation function!
-    hidden.map(sigmoid);
+    hidden.map(this.activation_function.func);
+
+    // Generating the output's output!
+    let output = Matrix.multiply(this.weights_ho, hidden);
+    output.add(this.bias_o);
+    output.map(this.activation_function.func);
+
+    // Sending back to the caller!
+    return output.toArray();
+  }
+
+  setLearningRate(learning_rate = 0.1) {
+    this.learning_rate = learning_rate;
+  }
+
+  setActivationFunction(func = sigmoid) {
+    this.activation_function = func;
+  }
+
+  train(input_array, target_array) {
+    // Generating the Hidden Outputs
+    let inputs = Matrix.fromArray(input_array);
+    let hidden = Matrix.multiply(this.weights_ih, inputs);
+    hidden.add(this.bias_h);
+    // activation function!
+    hidden.map(this.activation_function.func);
 
     // Generating the output's output!
     let outputs = Matrix.multiply(this.weights_ho, hidden);
     outputs.add(this.bias_o);
-    outputs.map(sigmoid);
+    outputs.map(this.activation_function.func);
 
     // Convert array to matrix object
     let targets = Matrix.fromArray(target_array);
@@ -61,7 +85,7 @@ class NeuralNetwork{
 
     // let gradient = outputs * (1 - outputs);
     // Calculate gradient
-    let gradients = Matrix.map(outputs, sigmoid);
+    let gradients = Matrix.map(outputs, this.activation_function.dfunc);
     gradients.multiply(output_errors);
     gradients.multiply(this.learning_rate);
 
@@ -80,7 +104,7 @@ class NeuralNetwork{
     let hidden_errors = Matrix.multiply(who_t, output_errors);
 
     // Calculate hidden gradient
-    let hidden_gradient = Matrix.map(hidden, sigmoid);
+    let hidden_gradient = Matrix.map(hidden, this.activation_function.dfunc);
     hidden_gradient.multiply(hidden_errors);
     hidden_gradient.multiply(this.learning_rate);
 
@@ -96,4 +120,5 @@ class NeuralNetwork{
     // targets.print();
     // error.print();
   }
+
 }
